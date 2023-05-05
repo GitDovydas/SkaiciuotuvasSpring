4c1d737e9f0e82f1020fd64fc024938222dab995
tomasito23 <125349254+tomasito23@users.noreply.github.com>
2023-05-05 08:58:41 +0300

    first commit

---
+++ src/main/java/com/spring/skaiciuotuvas/Skaiciuotuvas.java
@@ -0,0 +1,20 @@
+package com.spring.skaiciuotuvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Pagrindine klase, nes turi main metoda.
+//Spring boot application anotacija - yra lygi @Configuration, @EnableAutoConfiguration, @ComponentScan anotacijoms kartu sudejus.
+//Si anotacija nurodoma klaseje turincioje pagrindini (main metoda).
+@SpringBootApplication //anotacija
+public class Skaiciuotuvas {
+
+	public static void main(String[] args) {
+		//Programos kontrole deleguojama statiniam klases SpringApplication metodui run
+		//Nurodant aplikacijos saknini komponenta (SkaiciuotuvasApplication.class)
+		//Spring karkasas (framework) paleis aplikacija, tai yra startuos tomcat serveri su numatytais parametrais.
+		SpringApplication.run(Skaiciuotuvas.class, args);
+	}
+
+
+}
